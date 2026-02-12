04b2eb821f1fb5674116f5454b0f13e12222a5ee
adeebanaim492000 <adeebanaim49@gmail.com>
2026-02-13 00:09:07 +0530

    Initialize Cinema Club application with Spring Boot

---
+++ CinemaClubApplication.java
@@ -0,0 +1,13 @@
+package com.cinemaclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaClubApplication.class, args);
+    }
+}
+
